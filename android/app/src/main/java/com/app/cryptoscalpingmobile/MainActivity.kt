4044c912daac4e59fa18e8010699fbe5c10ac3c1
998993254467a-jpg <998993254467a@gmail.com>
2026-06-02 03:20:13 +0000

    fix: change package name to com.app.cryptoscalpingmobile (match v47), update versionCode/Name

--- android/app/src/main/java/com/app/cryptoscalpingmobile/MainActivity.kt
+++ android/app/src/main/java/com/app/cryptoscalpingmobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jtpower
+package com.app.cryptoscalpingmobile
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/app/cryptoscalpingmobile/MainApplication.kt
+++ android/app/src/main/java/com/app/cryptoscalpingmobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.jtpower
+package com.app.cryptoscalpingmobile
 
 import android.app.Application
 import android.content.res.Configuration
